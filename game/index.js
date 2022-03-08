68858fee25a0ae4d0758ac3aac55d320e7d62057
Nicholas Phillips <phillipssnicholas@gmail.com>
2022-03-08 11:04:53 -0700

    7 commit

--- game/index.js
+++ game/index.js
@@ -1,86 +1,98 @@
-const userContainer = document.querySelector('#user-info')
-const loginForm = document.querySelector('#login-form')
-const registerForm = document.querySelector('#register-form')
+const userContainer = document.querySelector("#user-info");
+const loginForm = document.querySelector("#login-form");
+const registerForm = document.querySelector("#register-form");
+const mooButton = document.querySelector("#intro")
 
-const baseURL = `http://127.0.0.1:5500/game/title.html`
+const baseURL = `http://localhost:3456/game`;
 
-const login = body => axios.post(`${baseURL}/login`, body).then( res => {
-  createUserCard(res.data)
-}).catch(err => {
-  console.log(err)
-  alert('MOOO! Your request failed.')
-})
-const register = body => axios.post(`${baseURL}/register`, body).then(res => {
-  createUserCard(res.data)
-}).catch(err => {
-  console.log(err)
-  alert('MOOO! Your request failed.')
-})
+const login = (body) =>
+  axios
+    .post(`${baseURL}/api/login`, body)
+    .then((res) => {
+      createUserCard(res.data)
+  window.location.href = "./intro.html" 
+    })
+    .catch((err) => {
+      console.log(err);
+      alert("MOOO! Your request failed.");
+    });
+const register = (body) =>
+  axios
+    .post(`${baseURL}/api/register`, body)
+    .then((res) => {
+      createUserCard(res.data);
+  window.location.href = "./intro.html" 
+    })
+    .catch((err) => {
+      console.log(err);
+      alert("MOOO! Your request failed.");
+    });
 
 function loginSubmitHandler(e) {
-    e.preventDefault()
+  e.preventDefault();
 
-    let username = document.querySelector('#login-username')
-    let password = document.querySelector('#login-password')
+  let username = document.querySelector("#login-username");
+  let password = document.querySelector("#login-password");
 
-    let bodyObj = {
-        username: username.value,
-        password: password.value
-    }
+  let bodyObj = {
+    username: username.value,
+    password: password.value,
+  };
 
-    login(bodyObj)
+  login(bodyObj);
 
-    username.value = ''
-    password.value = ''
+  username.value = "";
+  password.value = "";
+  window.location.href = "./intro.html" 
 }
 
 function registerSubmitHandler(e) {
-  e.preventDefault()
+  e.preventDefault();
 
-  let username = document.querySelector('#register-username')
-  let email = document.querySelector('#register-email')
-  let firstName = document.querySelector('#register-firstName')
-  let lastName = document.querySelector('#register-lastName')
-  let password = document.querySelector('#register-password')
-  let password2 = document.querySelector('#register-password-2')
+  let username = document.querySelector("#register-username");
+  let email = document.querySelector("#register-email");
+  let firstName = document.querySelector("#register-firstName");
+  let lastName = document.querySelector("#register-lastName");
+  let password = document.querySelector("#register-password");
+  let password2 = document.querySelector("#register-password-2");
 
   if (password.value !== password2.value) {
-    alert("Your passwords need to match.")
-    return
+    alert("Your passwords need to match. Moo!");
+    return;
   }
 
   let bodyObj = {
-      username: username.value,
-      email: email.value,
-      firstName: firstName.value,
-      lastName: lastName.value,
-      password: password.value
-  }
+    username: username.value,
+    email: email.value,
+    firstName: firstName.value,
+    lastName: lastName.value,
+    password: password.value,
+  };
 
-  register(bodyObj)
+  register(bodyObj);
 
-  username.value = ''
-  email.value = ''
-  firstName.value = ''
-  lastName.value = ''
-  password.value = ''
-  password2.value = ''
+  username.value = "";
+  email.value = "";
+  firstName.value = "";
+  lastName.value = "";
+  password.value = "";
+  password2.value = "";
+  // window.location.href = "./intro.html" 
 }
 
 function createUserCard(data) {
-    userContainer.innerHTML = ''
-    const userCard = document.createElement('div')
-    userCard.classList.add('user-card')
+  userContainer.innerHTML = "";
+  const userCard = document.createElement("div");
+  userCard.classList.add("user-card");
 
-    userCard.innerHTML = `<p class="username">Username: ${data.username}</p>
+  userCard.innerHTML = `<p class="username">Username: ${data.username}</p>
     <p class="email">Email: ${data.email}</p>
     <p class="first-name">First Name: ${data.firstName}</p>
     <p class="last-name">Last Name: ${data.lastName}</p>
-    `
+    `;
 
-
-    userContainer.appendChild(userCard)
+  userContainer.appendChild(userCard);
 }
 
-loginForm.addEventListener('submit', loginSubmitHandler)
-registerForm.addEventListener('submit', registerSubmitHandler)
+loginForm.addEventListener("submit", loginSubmitHandler);
+registerForm.addEventListener("submit", registerSubmitHandler);
---
+++ game/server/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config()
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const { login, register } = require("./controllers/auth");
+
+app.post(`/api/login`, login);
+app.post(`/api/register`, register);
+
+app.listen(3456, ()=>{
+    console.log("SERVER CONNECTED ON 3456")
+})
