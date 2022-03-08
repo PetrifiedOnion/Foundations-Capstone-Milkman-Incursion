676328bde126949010c9eb02bd5603269ce4513c
Nicholas Phillips <phillipssnicholas@gmail.com>
2022-03-07 17:00:35 -0700

    6 commit

---
+++ intro/app.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let doomButton = document.querySelector("#doom")
+function doomsDay() {
+    // doomButton.style.display = "none"
+//  setTimeout(3000)
+    doomButton.style.display = "flex"
+}
+setTimeout(doomsDay, 25000)
